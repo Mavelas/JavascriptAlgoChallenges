e769079fe7c435c59330ad5a6885b2dfee4c61ac
Erik Butz <erikbutzlive@gmail.com>
2023-12-18 20:22:35 -0600

    finished basic challenges 1

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,12 @@
-function countVowels() {}
+function countVowels(str) {
+  let counter = 0;
+  const formattedString = str.toLowerCase();
+  for (let i = 0; i < formattedString.length; i++) {
+    if (formattedString[i] === 'a' || formattedString[i] === 'e' || formattedString[i] === 'i' || formattedString[i] === 'o' || formattedString[i] === 'u') {
+      counter++;
+    }
+  }
+  return counter;
+}
 
 module.exports = countVowels;
--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,9 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  const removedDupes = new Set([]);
+  for (let index = 0; index < arr.length; index++) {
+    removedDupes.add(arr[index]);
+  }
+  return [...removedDupes];
+}
 
 module.exports = removeDuplicates;
