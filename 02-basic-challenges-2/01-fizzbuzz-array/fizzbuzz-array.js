ad8130f59b9d9c6740516b78f5e02b176e419d00
Erik Butz <erikbutzlive@gmail.com>
2023-12-19 20:30:03 -0600

    fizzbuzz & array intersections

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,12 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  const fizzBuzzArray = [];
+  for (let index = 1; index <= num; index++) {
+    if (index % 3 === 0 && index % 5 === 0) fizzBuzzArray.push("FizzBuzz");
+    else if (index % 3 === 0) fizzBuzzArray.push("Fizz");
+    else if (index % 5 === 0) fizzBuzzArray.push("Buzz");
+    else fizzBuzzArray.push(index);
+  }
+  return fizzBuzzArray;
+}
 
 module.exports = fizzBuzzArray;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,10 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const uniques = new Set([...arr1]);
+  const uniqueArr = [];
+  for (i = 0; i < arr2.length; i++) {
+    if (uniques.has(arr2[i])) uniqueArr.push(arr2[i]);
+  }
+  return uniqueArr;
+}
 
 module.exports = arrayIntersection;
